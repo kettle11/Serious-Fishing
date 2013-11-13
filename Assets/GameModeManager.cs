a935ba891c61dc6147111e1c271621713f85067c
Ryan Gimson <gimsor@rpi.edu>
2013-11-13 15:32:07 -0500

    Added Game Mode Manager and Work Scene for Ryan

---
+++ Assets/GameModeManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModeManager : MonoBehaviour {
+
+	enum GameState {
+		Menu = 0,
+		Fishing = 1,
+		Family = 2,
+		Story = 3
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
