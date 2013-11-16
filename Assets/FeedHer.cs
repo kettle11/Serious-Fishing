783097cf1ee1c6430d0f37188d212e4e5163554e
Cameron Chu <chuc3@rpi.edu>
2013-11-15 19:47:21 -0500

    Work on Getting Global Variables

---
+++ Assets/FeedHer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeedHer : MonoBehaviour {
+	bool alive = true;
+	// Use this for initialization
+	void Start () {
+
+	}
+	public GameObject thing;
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		if (alive){
+			Debug.Log(thing.wife.name);
+			//feed(wife)
+		}
+	}
+
+	void OnMouseEnter() {
+		if (alive)
+			gameObject.guiText.color = Color.red;
+	}
+	void OnMouseExit() {
+		if (alive)
+			gameObject.guiText.color = Color.black;
+	}
+}
