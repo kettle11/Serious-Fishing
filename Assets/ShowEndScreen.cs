58297cf352cbb3f494403d5016d8bfc21d5c1f5d
kettle11 <ian.kettlewell@gmail.com>
2013-12-04 13:16:12 -0500

    final changes

--- Assets/ShowEndScreen.cs
+++ Assets/ShowEndScreen.cs
@@ -3,22 +3,47 @@ using System.Collections;
 
 public class ShowEndScreen : MonoBehaviour {
 	
-	public Texture2D endMenuImage;
-
+	public Texture2D withDog;
+	public Texture2D withoutDog;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	float offset = 0; 
+
 	// Update is called once per frame
 	void Update () {
-		
+		if((offset + 100)  > -withDog.height - 1430)
+		{
+			offset -= Time.deltaTime * 100;
+		}
 	}
 
 	void OnGUI () {
-		Rect screenRect = new Rect(0,0,Screen.width,Screen.height);
 
-		GUI.DrawTexture(screenRect,endMenuImage,ScaleMode.ScaleToFit);
+		float scale = (float)Screen.width / (float)withDog.width;
+	
+		float y = offset;
+		if(offset > -100)
+		{
+			y = 0;
+		}
+		else
+		{
+			y = offset + 100;
+		}
+
+		Rect screenRect = new Rect(0,y,withDog.width * scale,withDog.height * scale);
+
+		if(FamilySceneGlobal.dog.alive)
+		{
+			GUI.DrawTexture(screenRect,withDog);
+		}
+		else
+		{
+			GUI.DrawTexture(screenRect,withoutDog);
+		}
 	}
 
 }
