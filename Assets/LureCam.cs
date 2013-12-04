c258c71ffea131f2c9a67cf40b0c16354a751ce7
kettle11 <ian.kettlewell@gmail.com>
2013-12-04 07:43:25 -0500

    Hook 2D

--- Assets/LureCam.cs
+++ Assets/LureCam.cs
@@ -72,13 +72,28 @@ public class LureCam : MonoBehaviour {
 	//The lure this camera is following
 	public Transform target;
 
+	public Texture texture;
+
+	Vector2 hookPoint;
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(hookPoint.x - 10, hookPoint.y - 15, 20, 30),texture); 
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if(target.transform.position.y < startY)
 		{
 			transform.position = new Vector3(transform.position.x, target.transform.position.y + target.rigidbody.velocity.y * .2f, transform.position.z);
 
+
+
+
 		}
+
+		hookPoint = camera.WorldToScreenPoint(target.position);
+		hookPoint = new Vector2(hookPoint.x, camera.pixelHeight - hookPoint.y);
 
 		//transform.LookAt(target.transform.position);
 
