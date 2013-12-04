49722ab7d67689038f4b70007ac57641c5f36e23
kettle11 <ian.kettlewell@gmail.com>
2013-12-04 12:02:01 -0500

    Changes

---
+++ Assets/ShowEndScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowEndScreen : MonoBehaviour {
+	
+	public Texture2D endMenuImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI () {
+		Rect screenRect = new Rect(0,0,Screen.width,Screen.height);
+
+		GUI.DrawTexture(screenRect,endMenuImage,ScaleMode.ScaleToFit);
+	}
+
+}
